51a0ee430c51d1c2b020d0fcef537f19d43723b4
orleven <546577246@qq.com>
2020-11-11 20:47:38 +0800

    [fix] fix script bug
    [fix] fix script bug

--- script/druid/druid_monitor_unauth.py
+++ script/druid/druid_monitor_unauth.py
@@ -1,51 +1,3 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @author: 'orleven'
@@ -54,7 +6,7 @@ from lib.utils.connect import ClientSession
 from lib.core.enums import VUL_LEVEL
 from lib.core.enums import VUL_TYPE
 from lib.core.enums import SERVICE_PORT_MAP
-from script import Script, VUL_TYPE, VUL_LEVEL
+from script import Script
 
 class POC(Script):
     def __init__(self, target=None):
